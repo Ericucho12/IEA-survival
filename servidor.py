0d79028f7285d0ab783ce254c142649a04b80366
Ericucho12 <ericcmelo543@gmail.com>
2024-05-12 21:12:24 +0000

    Mundo guardado 2024-05-12

--- servidor.py
+++ servidor.py
@@ -16,7 +16,7 @@
 # eu		  Europe (Frankfurt)
 # in		  India (Mumbai)
 # jp		  Japan (Tokyo)
-# sa		  South America (São Paulo)
+# sa		  South Ame1rica (São Paulo)
 # us		  United States (Ohio)
 # us-cal-1	  United States (California)
 
